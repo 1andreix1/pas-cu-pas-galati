8ad93cdf176890906f1bb2a4044cc4ec8d2d492e
1andreix1 <163720441+1andreix1@users.noreply.github.com>
2024-03-24 12:23:58 +0200

    Update documente.php

--- documente.php
+++ documente.php
@@ -1,10 +1,15 @@
 <?php
-$uploadDirectory = 'uploads/'; // Directorul în care sunt salvate fișierele încărcate
+// documents.php - Script PHP pentru afișarea listei de documente disponibile pe server
 
-$documents = scandir($uploadDirectory);
-foreach ($documents as $document) {
-    if ($document != '.' && $document != '..') {
-        echo "<li><a href='$uploadDirectory$document'>$document</a></li>";
+$targetDirectory = "uploads/"; // Directorul în care sunt stocate fișierele încărcate
+
+// Obțineți lista fișierelor din director
+$files = glob($targetDirectory . "*");
+
+// Afișați lista de fișiere ca linkuri
+foreach ($files as $file) {
+    if (is_file($file)) {
+        echo "<li><a href='" . $file . "' download>" . basename($file) . "</a></li>";
     }
 }
 ?>
